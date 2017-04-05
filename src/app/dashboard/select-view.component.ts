977cb52553870ff5828334cb4926b59a09971af2
anjana <anjana@qburst.com>
2017-04-05 16:51:30 +0530

    event binding btw select and dashboard component

--- src/app/dashboard/select-view.component.ts
+++ src/app/dashboard/select-view.component.ts
@@ -1,16 +1,19 @@
-import { Component, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'select-view',
   templateUrl: './select-view.component.html'
 })
 export class SelectViewComponent {
-  selectedView = 'Employees';
+  @Input() selectedView = 'Employees';
+  @Output() viewChanged = new EventEmitter<string> ();
+
   filterDetails = [
     'Employees',
     'Projects'
   ];
-  changeView(selectedValue) {
+  changeView(selectedValue: string) {
     console.log(selectedValue);
+    this.viewChanged.emit(selectedValue);
   }
 }
