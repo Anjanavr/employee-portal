c20b6a71289a365fb46baa5f9d1beae6f0cec987
anjana <anjana@qburst.com>
2017-04-07 15:14:06 +0530

    added table compoenent and rendered data from jsons

--- src/app/dashboard/select-view.component.ts
+++ src/app/dashboard/select-view.component.ts
@@ -5,14 +5,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './select-view.component.html'
 })
 export class SelectViewComponent {
-  @Input() selectedView = 'Employees';
+  @Input() selectedView;
   @Output() viewChanged = new EventEmitter<string> ();
 
+  constructor() {
+    this.selectedView = '';
+  }
+
   filterDetails = [
-    'Employees',
-    'Projects'
+    'employees',
+    'projects'
   ];
   changeView(selectedValue: string) {
     this.viewChanged.emit(selectedValue);
+  }
+  ngOnInit() {
+    this.selectedView = 'employees';
+    this.changeView('employees');
   }
 }
--- src/app/table-component/table-componnent.component.ts
+++ src/app/table-component/table-componnent.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'table-component',
-  templateUrl: './table-component.component.html'
+  templateUrl: './table-component.component.html',
+  styleUrls: ['./table-component.component.css']
 })
 export class TableComponent {
 
+  @Input() config;
+
+  @Input() isDataAvailable;
 }
